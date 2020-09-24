54666b9530b988725ca7a61ec243fbc70826e533
Aniket Kumar Verma <vermaaniket213@gmail.com>
2020-09-24 19:43:53 +0530

    header correct

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import {Link} from "react-router-dom";
+import CallIcon from '@material-ui/icons/Call';
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
 class Header extends Component {
     state = {  }
     render() { 
         return ( 
             <div className="header-container">
                 <div className="header-content">
-                    <div>Nandi Sewa Sansthan</div>
+                    <div className="header-content-container">
+                    <span><CallIcon fontSize="large"/>+91-1234567890</span>
+                    <span className="header-space"><MailOutlineIcon fontSize="large"/>admin@nandisewa.org,</span>
+                    <span className="header-space"><MailOutlineIcon fontSize="large"/>member@nandisewa.org</span>
+                    <span className="header-space">Nandi Sewa Sansthan</span></div>
                     <div className="hindi-text">परोSपि हितवान् बन्धु</div>
-                    </div>
+                </div>
                 <div className="button-container">
                     <div><Link className="donate-link" to="/donate"><button className="btn btn-light button-donate">Donate Now</button></Link></div>
                     <div className="volunteer-button-container"><Link className="volunteers-link" to="/volunteers"><button className="btn btn-danger button-volunteer">Become a volunteer</button></Link></div>
